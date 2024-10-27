3f315c484472d484abbdb07aaed0bfdeeb0f8ef2
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-27 21:33:44 +0700

    added stats

--- Three.js-TypeScript-Boilerplate/src/main.ts
+++ Three.js-TypeScript-Boilerplate/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
 
@@ -18,17 +19,23 @@ window.addEventListener('resize', () => {
 })
 
 new OrbitControls(camera, renderer.domElement)
+
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
 function animate() {
   requestAnimationFrame(animate)
 
   cube.rotation.x += 0.001
   cube.rotation.y += 0.001
+
+  stats.update()
 
   renderer.render(scene, camera)
 }
